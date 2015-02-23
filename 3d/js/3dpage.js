6254fd7d31fea571b2985d941f1493d5239940d4
Ivan Chasov <chasow@yandex.ru>
2015-02-23 22:15:29 +0300

    отрабатываем движение квадратиков и их размерность

--- 3d/js/3dpage.js
+++ 3d/js/3dpage.js
@@ -1,3 +1,11 @@
+var menu_boxes_options = [
+	{left: 10, top: 20, size: 15, color: '#002143', bordercolor: '#00162C'}
+	, {left: 20, top: 40, size: 15, color: '#002143', bordercolor: '#00162C'}
+	, {left: 35, top: 15, size: 15, color: '#002143', bordercolor: '#00162C'}
+	, {left: 45, top: 35, size: 15, color: '#002143', bordercolor: '#00162C'}
+	, {left: 60, top: 10, size: 15, color: '#002143', bordercolor: '#00162C'}
+];
+var menu_all_boxes = new Object();
 var winWidth = 0, winHeight = 0;
 
 var rotation = 0;
@@ -9,6 +17,10 @@ function ResizeMain(){
 	
 	$('body').css({backgroundSize: winWidth + 'px ' + winHeight + 'px'});
 	
+	for (var k in menu_all_boxes){
+		menu_all_boxes[k].resizeOneBox();
+	}
+
 }
 
 function create_menu(){
@@ -17,15 +29,41 @@ function create_menu(){
 	TweenLite.set($('body'), {perspective: 500});
 	//offset the origin on the z-axis to make the spins more interesting.
 	//TweenLite.set($('#testb'), {transformOrigin:"center center -150px"});
-	menu_one_box('20%',(winHeight / 100 * 20) + 'px','200','200', '#002143', '#00162C');
-	menu_one_box('30%',(winHeight / 100 * 40) + 'px','200','200', '#002143', '#00162C');
+	
+	//menu_all_boxes = {box1: new menu_one_box(20,20,20,20, '#002143', '#00162C'), box2: new menu_one_box(30,40,20,20, '#002143', '#00162C'), box3: new menu_one_box(40,15,20,20, '#002143', '#00162C')
+	//	, box4: new menu_one_box(50,45,20,20, '#002143', '#00162C')
+	//};
+	for (var i = 0; i < menu_boxes_options.length; i++)
+	{
+		menu_all_boxes['box' + i] = new menu_one_box(menu_boxes_options[i].left, menu_boxes_options[i].top, menu_boxes_options[i].size, '#002143', '#00162C');
+	}
 }
 
-menu_one_box = function(_left, _top, _width, _height, _color, _border){
-	var ignoreRollovers = false;
+menu_one_box = function(_left, _top, _size, _color, _border){
 	countBox++;
+	this.left = _left;
+	this.top = _top;
+	this.size = _size;
+	this.namebox = '#onebox' + countBox;
+	
+	var ignoreRollovers = false;
+	this.sizex = 0;
+	this.sizey = 0;
+	
+	this.SetSizeXY = function(){
+		if (winWidth > winHeight){
+			this.sizex = winWidth / 100 * this.size;
+			this.sizey = this.sizex * 0.92;
+		} else {
+			this.sizey = winHeight / 100 * this.size;
+			this.sizex = this.sizey * 0.92;
+		}
+	}
+	
+	this.SetSizeXY();
+	
 	$('body').append('<a href="http://yandex.ru"><div id="onebox' + countBox + '" class="onebox">654654</div></a>');
-	$('#onebox' + countBox).css({left: _left, top: _top, width: _width, height: _height, backgroundColor: _color, border: '1px solid ' + _border
+	$(this.namebox).css({left: _left + '%', top: winHeight / 100 * _top + 'px', width: this.sizex + 'px', height: this.sizey + 'px', backgroundColor: _color, border: '1px solid ' + _border
 		}).hover(function() {
 			if (true) {
 				rotation += 360;
@@ -37,9 +75,15 @@ menu_one_box = function(_left, _top, _width, _height, _color, _border){
 			}
 	}, function(){});
 	//pulsate the box using scaleX and scaleY
-	TweenMax.to($('#onebox' + countBox), Math.random() * (2.1 - 1.88) + 1.88, {scaleX:Math.random() * (0.95 - 0.88) + 0.88, scaleY:Math.random() * (0.95 - 0.88) + 0.88, force3D:true, yoyo:true, repeat:-1, ease:Power1.easeInOut});
+	TweenMax.to($(this.namebox), Math.random() * (2.7 - 1.88) + 1.88, {scaleX:Math.random() * (0.95 - 0.88) + 0.88, scaleY:Math.random() * (0.95 - 0.88) + 0.88, force3D:true, yoyo:true, repeat:-1, ease:Power1.easeInOut});
 	
-	resizeOneBox = function(){
-		
+	this.resizeOneBox = function(){
+		this.SetSizeXY();
+		$(this.namebox).css({top: winHeight / 100 * this.top + 'px', width: this.sizex + 'px', height: this.sizey + 'px'});
 	}
+
+	return this;
 }
+
+
+
